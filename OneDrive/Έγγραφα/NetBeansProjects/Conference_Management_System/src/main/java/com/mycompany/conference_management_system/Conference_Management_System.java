2f326974564cc8491ca58cb5636bb82daf0d4b8b
George Zachos <giorgoszaxos2003@gmail.com>
2023-12-20 20:51:49 +0200

    initial commit

---
+++ OneDrive/Έγγραφα/NetBeansProjects/Conference_Management_System/src/main/java/com/mycompany/conference_management_system/Conference_Management_System.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.conference_management_system;
+
+/**
+ *
+ * @author giorg
+ */
+public class Conference_Management_System {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
---
+++ OneDrive/Έγγραφα/NetBeansProjects/Conference_Management_System/src/test/java/TestT.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/UnitTests/JUnit5TestClass.java to edit this template
+ */
+
+import jdk.jfr.Description;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.AfterAll;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+/**
+ *
+ * @author giorg
+ */
+public class TestT {
+    @Test
+    @Description("Test 1")
+    void mainTest()
+    {
+        assertEquals(1,1);
+    }
+}
